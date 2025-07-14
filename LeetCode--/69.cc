5d1ed497e4a3fce3c0cc07a7b0230a2ab4030717
linnaid <jiaguoyangyang@qq.com>
2025-07-14 20:56:43 +0800

    mySqrt

---
+++ LeetCode--/69.cc
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int mySqrt(int x) {
+        if(x == 0)
+        return x;
+        for(long long i = 1; i <= x; i++){
+            if((i * i) > x)
+            return i-1;
+        }
+        return x;
+    }
+};
